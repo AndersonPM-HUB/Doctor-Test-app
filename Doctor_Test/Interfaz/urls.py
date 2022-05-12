ce407b2dbce3285807700295ca64c84b7f5b2c04
AndersonPM-HUB <79481049+AndersonPM-HUB@users.noreply.github.com>
2022-05-11 21:42:14 -0500

    se conecta webservice a la interfaz

--- Doctor_Test/Interfaz/urls.py
+++ Doctor_Test/Interfaz/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns =[
     path('', views.interfaz, name='interfaz'), 
-    path('plataforma/', views.plataforma, name='plataforma'), 
-     
+    path('plataforma/', views.plataforma, name='plataforma'),
+    path('pacientes/', views.pacientes, name='pacientes'), 
+    
+ 
 ]
--- Doctor_Test/Interfaz/views.py
+++ Doctor_Test/Interfaz/views.py
@@ -1,11 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import requests
 
-# Create your views here.
+
 def interfaz(request):
     return render(request, 'index.html')
-
 
 @login_required
 def plataforma(request):
     return render(request, 'plataforma.html')
+
+@login_required
+def pacientes(request):
+    response = requests.get('http://127.0.0.1:8000/pacientes/api/api/paciente/')
+    pacientes = response.json()
+    return render(request, 'pacientes.html',  {'pacientes': pacientes})
+
